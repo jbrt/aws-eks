d16986952d0d0e5ea451d7917d944d595db3438b
Julien B <julien@toshokan.fr>
2019-02-03 18:06:23 +0100

    Adding Auto-Scaling policy and alarm to the existing ASG (not done before)

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,4 +1,6 @@
 # Creating an EKS cluster
+# - EKS Cluster and workers
+# - ASG Policy & Alarm for auto-scaling
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -19,4 +21,30 @@ module "eks" {
   ]
 
   tags = "${local.tags}"
+}
+
+# Define the Auto-Scaling Policy & Alarm
+resource "aws_autoscaling_policy" "eks-autoscaling-policy" {
+  name                   = "eks-autoscaling-policy"
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = 2
+  cooldown               = 300
+  autoscaling_group_name = "${module.eks.workers_asg_names[0]}"
+}
+resource "aws_cloudwatch_metric_alarm" "eks-asg-alarm" {
+  alarm_name          = "eks-autoscaling-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "70"
+
+  dimensions = {
+    AutoScalingGroupName = "${module.eks.workers_asg_names[0]}"
+  }
+
+  alarm_description = "This metric monitors EKS workers ec2 instances cpu utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.eks-autoscaling-policy.arn}"]
 }
