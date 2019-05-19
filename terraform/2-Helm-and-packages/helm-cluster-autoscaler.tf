c8cf33b2784a5e225a65a9e836b0d127762b600f
Julien B <julien@toshokan.fr>
2019-05-19 18:15:19 +0200

    Adding support of cluster-autoscaler (without HELM for the moment)

--- terraform/2-Helm-and-packages/helm-cluster-autoscaler.tf
+++ terraform/2-Helm-and-packages/helm-cluster-autoscaler.tf
@@ -1,7 +1,22 @@
 # Cluster-autoscaler
 
-resource "helm_release" "cluster-autoscaler" {
-  depends_on = ["kubernetes_cluster_role_binding.tiller-rights"]
-  name       = "cluster-autoscaler"
-  chart      = "stable/cluster-autoscaler"
+# Using a non HELM package for the moment (because I had some problems with)
+
+data "template_file" "auto-scaler" {
+  template = "${file("${path.module}/templates/cluster-autoscaler.tpl")}"
+  vars = {
+    VERSION = "1.12.3"
+    CLUSTER = "${var.cluster_name}"
+  }
+}
+
+resource "local_file" "cluster-autoscaler" {
+  filename = "${path.module}/templates/cluster-autoscaler.yml"
+  content  = "${data.template_file.auto-scaler.rendered}"
+
+  depends_on = ["helm_release.metrics-server"]
+
+  provisioner "local-exec" {
+    command = "kubectl --kubeconfig ${path.module}/../1-Create-EKS-Cluster/kubeconfig_${var.cluster_name} apply -f ${path.module}/templates/cluster-autoscaler.yml"
+  }
 }
