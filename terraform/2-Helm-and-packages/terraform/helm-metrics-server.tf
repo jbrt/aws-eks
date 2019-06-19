69a0b4f7edd18418890a2ec92fa749230265f57f
Julien B <julien@toshokan.fr>
2019-06-19 17:53:54 +0200

    Updating metrics-server installation

--- terraform/2-Helm-and-packages/terraform/helm-metrics-server.tf
+++ terraform/2-Helm-and-packages/terraform/helm-metrics-server.tf
@@ -4,4 +4,5 @@ resource "helm_release" "metrics-server" {
   depends_on = ["kubernetes_cluster_role_binding.tiller-rights"]
   name       = "metrics-server"
   chart      = "stable/metrics-server"
+  namespace  = "kube-system"
 }
